a89edd04690d7a4016460121e5ad65c83f4f52ce
Philipp Stracker <stracker.phil@gmail.com>
2024-02-09 23:30:30 +0100

    Rewrite the plugin logic

--- main.js
+++ main.js
@@ -1,178 +1,394 @@
-const { App, Editor, EditorSuggest, TFile, Notice, Plugin, PluginSettingTab, Setting } = require('obsidian')
+const {
+    EditorSuggest,
+    Plugin,
+    PluginSettingTab,
+    Setting
+} = require('obsidian')
 
 const DEFAULT_SETTINGS = {
-	peopleFolder: 'People/',
-	// Defaults:
-	// useExplicitLinks: undefined,
-	// useLastNameFolder: undefined,
+    mentionTypes: [],
+
+    // matchStart: undefined,
 }
 
-const NAME_REGEX = /\/@([^\/]+)\.md$/
-const LAST_NAME_REGEX = /([\S]+)$/
+const getLink = (path, settings) => {
+    if (!path.endsWith('.md')) {
+        return null;
+    }
 
-const getPersonName = (filename, settings) => filename.startsWith(settings.peopleFolder)
-	&& filename.endsWith('.md')
-	&& filename.includes('/@')
-	&& NAME_REGEX.exec(filename)?.[1]
+    for (let i = 0; i < settings.mentionTypes.length; i++) {
+        const type = settings.mentionTypes[i];
+
+        if (!type?.sign) {
+            continue;
+        }
+
+        if (!path.includes('/' + type.sign)) {
+            continue;
+        }
+
+        const regex = new RegExp(`/${type.sign}([^/]+)\\.md$`);
+        let result = regex.exec(path)
+
+        if (result?.[1]) {
+            return {
+                sign: type.sign,
+                name: result[1],
+                path
+            }
+        }
+    }
+
+    return null;
+}
+
+function getTypeDef(types, sign) {
+    for (let i = 0; i < types.length; i++) {
+        if (sign === types[i]?.sign) {
+            return types[i];
+        }
+    }
+
+    return null;
+}
 
 module.exports = class AtPeople extends Plugin {
-	async onload() {
-		await this.loadSettings()
-		this.registerEvent(this.app.vault.on('delete', async event => { await this.update(event) }))
-		this.registerEvent(this.app.vault.on('create', async event => { await this.update(event) }))
-		this.registerEvent(this.app.vault.on('rename', async (event, originalFilepath) => { await this.update(event, originalFilepath) }))
-		this.addSettingTab(new AtPeopleSettingTab(this.app, this))
-		this.suggestor = new AtPeopleSuggestor(this.app, this.settings)
-		this.registerEditorSuggest(this.suggestor)
-		this.app.workspace.onLayoutReady(this.initialize)
-	}
+    async onload() {
+        await this.loadSettings()
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
-	}
+        this.registerEvent(this.app.vault.on('delete', async event => {
+            await this.update(event)
+        }))
+        this.registerEvent(this.app.vault.on('create', async event => {
+            await this.update(event)
+        }))
+        this.registerEvent(this.app.vault.on('rename', async (event, originalFilepath) => {
+            await this.update(event, originalFilepath)
+        }))
 
-	async saveSettings() {
-		await this.saveData(this.settings || DEFAULT_SETTINGS)
-	}
+        this.addSettingTab(new AtPeopleSettingTab(this.app, this))
+        this.suggestor = new AtPeopleSuggestor(this.app, this.settings)
+        this.registerEditorSuggest(this.suggestor)
+        this.app.workspace.onLayoutReady(this.initialize)
+    }
 
-	updatePeopleMap = () => {
-		this.suggestor.updatePeopleMap(this.peopleFileMap)
-	}
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData()
+        )
+    }
 
-	update = async ({ path, deleted, ...remaining }, originalFilepath) => {
-		this.peopleFileMap = this.peopleFileMap || {}
-		const name = getPersonName(path, this.settings)
-		let needsUpdated
-		if (name) {
-			this.peopleFileMap[name] = path
-			needsUpdated = true
-		}
-		originalFilepath = originalFilepath && getPersonName(originalFilepath, this.settings)
-		if (originalFilepath) {
-			delete this.peopleFileMap[originalFilepath]
-			needsUpdated = true
-		}
-		if (needsUpdated) this.updatePeopleMap()
-	}
+    async saveSettings() {
+        await this.saveData(this.settings || DEFAULT_SETTINGS)
+    }
 
-	initialize = () => {
-		this.peopleFileMap = {}
-		for (const filename in this.app.vault.fileMap) {
-			const name = getPersonName(filename, this.settings)
-			if (name) this.peopleFileMap[name] = filename
-		}
-		window.setTimeout(() => {
-			this.updatePeopleMap()
-		})
-	}
+    refreshFileList = () => {
+        this.suggestor.setSuggestionsMap(this.fileMaps)
+    }
+
+    update = async ({path, deleted, ...remaining}, originalFilepath) => {
+        let needsUpdated = false;
+
+        this.fileMaps = this.fileMaps || {}
+
+        const addItem = getLink(path, this.settings)
+
+        if (addItem) {
+            this.addFileToMap(addItem)
+            needsUpdated = true
+        }
+
+        const removeItem = originalFilepath && getLink(originalFilepath, this.settings)
+        if (removeItem) {
+            this.removeFileFromMap(removeItem)
+            needsUpdated = true
+        }
+
+        if (needsUpdated) {
+            this.refreshFileList()
+        }
+    }
+
+    initialize = () => {
+        this.fileMaps = {}
+
+        for (const filename in this.app.vault.fileMap) {
+            const addItem = getLink(filename, this.settings)
+
+            if (addItem) {
+                this.addFileToMap(addItem)
+            }
+        }
+
+        window.setTimeout(this.refreshFileList, 0, this)
+    }
+
+    addFileToMap(item) {
+        const sign = item?.sign;
+        
+        if (!sign) {
+            return;
+        }
+
+        const key = item.name.toLowerCase();
+        this.fileMaps[sign] = this.fileMaps[sign] || {};
+        this.fileMaps[sign][key] = item;
+    }
+
+    removeFileFromMap(item) {
+        const sign = item?.sign;
+
+        if (!sign) {
+            return;
+        }
+
+        if (this.fileMaps[sign]) {
+            const key = item.name.toLowerCase();
+            delete this.fileMaps[sign][key]
+        }
+    }
 }
 
+
 class AtPeopleSuggestor extends EditorSuggest {
-	constructor(app, settings) {
-		super(app)
-		this.settings = settings
-	}
-	updatePeopleMap(peopleFileMap) {
-		this.peopleFileMap = peopleFileMap
-	}
-	onTrigger(cursor, editor, tFile) {
-		let charsLeftOfCursor = editor.getLine(cursor.line).substring(0, cursor.ch)
-		let atIndex = charsLeftOfCursor.lastIndexOf('@')
-		let query = atIndex >= 0 && charsLeftOfCursor.substring(atIndex + 1)
-		if (
-			query
-			&& !query.includes(']]')
-			&& (
-				// if it's an @ at the start of a line
-				atIndex === 0
-				// or if there's a space character before it
-				|| charsLeftOfCursor[atIndex - 1] === ' '
-			)
-		) {
-			return {
-				start: { line: cursor.line, ch: atIndex },
-				end: { line: cursor.line, ch: cursor.ch },
-				query,
-			}
-		}
-		return null
-	}
-	getSuggestions(context) {
-		let suggestions = []
-		for (let key in (this.peopleFileMap || {}))
-			if (key.toLowerCase().startsWith(context.query))
-				suggestions.push({
-					suggestionType: 'set',
-					displayText: key,
-					context,
-				})
-		suggestions.push({
-			suggestionType: 'create',
-			displayText: context.query,
-			context,
-		})
-		return suggestions
-	}
-	renderSuggestion(value, elem) {
-		if (value.suggestionType === 'create') elem.setText('New person: ' + value.displayText)
-		else elem.setText(value.displayText)
-	}
-	selectSuggestion(value) {
-		let link
-		if (this.settings.useExplicitLinks && this.settings.useLastNameFolder) {
-			let lastName = LAST_NAME_REGEX.exec(value.displayText)
-			lastName = lastName && lastName[1] && (lastName[1] + '/') || ''
-			link = `[[${this.settings.peopleFolder}${lastName}@${value.displayText}.md|@${value.displayText}]]`
-		} else if (this.settings.useExplicitLinks && !this.settings.useLastNameFolder) {
-			link = `[[${this.settings.peopleFolder}@${value.displayText}.md|@${value.displayText}]]`
-		} else {
-			link = `[[@${value.displayText}]]`
-		}
-		value.context.editor.replaceRange(
-			link,
-			value.context.start,
-			value.context.end,
-		)
-	}
+    constructor(app, settings) {
+        super(app)
+        this.settings = settings
+        this.currSign = '';
+    }
+
+    setSuggestionsMap(maps) {
+        this.maps = maps;
+    }
+
+    onTrigger(cursor, editor, tFile) {
+        const usedSigns = this.settings.mentionTypes.map(object => object.sign);
+
+        let charsLeftOfCursor = editor.getLine(cursor.line).substring(0, cursor.ch)
+
+        if (!charsLeftOfCursor) {
+            return null;
+        }
+
+        let signIndex = -1;
+
+        for (let sign of usedSigns) {
+            let index = charsLeftOfCursor.lastIndexOf(sign);
+
+            if (index !== -1 && index > signIndex) {
+                signIndex = index;
+                this.currSign = sign;
+            }
+        }
+
+        if (signIndex < 0) {
+            return null;
+        }
+
+        let query = signIndex >= 0 && charsLeftOfCursor.substring(signIndex + 1)
+
+        if (
+            query
+            && !query.includes(']]')
+            && (
+                // if it's an @ at the start of a line
+                signIndex === 0
+                // or if there's a space character before it
+                || charsLeftOfCursor[signIndex - 1] === ' '
+            )
+        ) {
+            return {
+                start: {line: cursor.line, ch: signIndex},
+                end: {line: cursor.line, ch: cursor.ch},
+                query,
+            }
+        }
+
+        return null
+    }
+
+    getSuggestions(context) {
+        let suggestions = []
+        let map = this.maps[this.currSign] || {};
+
+        for (let key in map) {
+            let isMatch;
+
+            if (!key) {
+                continue;
+            }
+
+            const term = context.query.toLowerCase()
+
+            if (this.settings.matchStart) {
+                isMatch = key.startsWith(term)
+            } else {
+                isMatch = key.includes(term);
+            }
+
+            if (isMatch) {
+                suggestions.push({
+                    suggestionType: 'set',
+                    displayText: map[key].name.trim(),
+                    linkName: map[key].name,
+                    context,
+                })
+            }
+        }
+
+        suggestions.push({
+            suggestionType: 'create',
+            displayText: context.query,
+            linkName: context.query,
+            context,
+        })
+
+        return suggestions
+    }
+
+    renderSuggestion(value, elem) {
+        if (value.suggestionType === 'create') {
+            const type = getTypeDef(this.settings.mentionTypes, this.currSign);
+            const label = type?.label || 'Item';
+
+            elem.setText(`Create ${label}: ${value.displayText}`)
+        } else {
+            elem.setText(value.displayText)
+        }
+    }
+
+    selectSuggestion(value) {
+        let link
+
+        console.log(value);
+        link = `[[${this.currSign}${value.linkName}]]`
+
+        value.context.editor.replaceRange(
+            link,
+            value.context.start,
+            value.context.end,
+        )
+    }
+
+}
+
+
+// ----------------------------------------------------------------------------
+// Settings
+
+/**
+ * @param {string[]} usedSigns
+ * @param {string} currentSign
+ * @returns {object}
+ */
+function getAvailableSigns(usedSigns, currentSign) {
+    const allowedSigns = [
+        '@',
+        '%',
+        '&',
+        '!',
+        '?',
+        '+',
+        ';',
+        '.',
+        '=',
+        '^',
+        '§',
+        '$',
+        '-',
+        '_',
+        '(',
+        '{',
+    ];
+
+    const availableSigns = {};
+
+    allowedSigns.forEach(sign => {
+        if (currentSign === sign || !usedSigns.includes(sign)) {
+            availableSigns[sign] = `${sign}Name`
+        }
+    })
+
+    return availableSigns;
 }
 
 class AtPeopleSettingTab extends PluginSettingTab {
-	constructor(app, plugin) {
-		super(app, plugin)
-		this.plugin = plugin
-	}
-	display() {
-		const { containerEl } = this
-		containerEl.empty()
-		new Setting(containerEl)
-			.setName('People folder')
-			.setDesc('The folder where people files live, e.g. "People/". (With trailing slash.)')
-			.addText(
-				text => text
-					.setPlaceholder(DEFAULT_SETTINGS.peopleFolder)
-					.setValue(this.plugin.settings.peopleFolder)
-					.onChange(async (value) => {
-						this.plugin.settings.peopleFolder = value
-						await this.plugin.saveSettings()
-					})
-			)
-		new Setting(containerEl)
-			.setName('Explicit links')
-			.setDesc('When inserting links include the full path, e.g. [[People/@Bob Dole.md|@Bob Dole]]')
-			.addToggle(
-				toggle => toggle.onChange(async (value) => {
-					this.plugin.settings.useExplicitLinks = value
-					await this.plugin.saveSettings()
-				})
-			)
-		new Setting(containerEl)
-			.setName('Last name folder')
-			.setDesc('When using explicit links, use the "last name" (the last non-spaced word) as a sub-folder, e.g. [[People/Dole/@Bob Dole.md|@Bob Dole]]')
-			.addToggle(
-				toggle => toggle.onChange(async (value) => {
-					this.plugin.settings.useLastNameFolder = value
-					await this.plugin.saveSettings()
-				})
-			)
-	}
+    constructor(app, plugin) {
+        super(app, plugin)
+        this.plugin = plugin
+    }
+
+
+    display() {
+        const {containerEl} = this
+        let usedSigns = this.plugin.settings.mentionTypes.map(object => object.sign);
+
+        containerEl.empty()
+
+        containerEl.createEl("h2", {text: "Mention Things"});
+
+        this.plugin.settings.mentionTypes.forEach((value, index) => {
+            const setting = new Setting(containerEl)
+            const availableSigns = getAvailableSigns(usedSigns, value?.sign);
+
+            setting.addDropdown(
+                list => list
+                    .addOptions(availableSigns)
+                    .setValue(value?.sign || '')
+                    .onChange(async (value) => {
+                        this.plugin.settings.mentionTypes[index].sign = value
+                        await this.plugin.saveSettings()
+                        this.display()
+                    })
+            )
+
+            setting.addText(
+                text => text
+                    .setPlaceholder('Label. Example "Person"')
+                    .setValue(value?.label || '')
+                    .onChange(async (value) => {
+                        this.plugin.settings.mentionTypes[index].label = value
+                        await this.plugin.saveSettings()
+                    })
+                    .inputEl.addClass('type_label')
+            )
+
+            setting.addExtraButton(
+                button => button
+                    .setIcon("cross")
+                    .setTooltip("Delete")
+                    .onClick(async () => {
+                        this.plugin.settings.mentionTypes.splice(index, 1);
+                        await this.plugin.saveSettings();
+                        this.display();
+                    })
+            );
+
+            setting.infoEl.remove();
+        });
+
+        new Setting(containerEl)
+            .addButton(cb => {
+                cb.setButtonText("New Type")
+                    .setCta()
+                    .onClick(async () => {
+                        this.plugin.settings.mentionTypes.push({});
+                        await this.plugin.saveSettings();
+                        this.display();
+                    });
+            });
+
+        new Setting(containerEl)
+            .setName('Match from start')
+            .setDesc('Whether to suggest only items that start with the search term. When disabled, any part of the name can match the search term')
+            .addToggle(
+                toggle => toggle.setValue(this.plugin.settings.matchStart)
+                    .onChange(async (value) => {
+                        this.plugin.settings.matchStart = value
+                        await this.plugin.saveSettings()
+                    })
+            )
+    }
 }
